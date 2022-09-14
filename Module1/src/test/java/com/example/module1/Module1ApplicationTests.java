d156c3ad154d2192e3c9a14c1e7d540ee5909eb0
turkuaz.sengul <turkuazsengul@gmail.com>
2022-09-14 23:51:16 +0300

    first commit

---
+++ Module1/src/test/java/com/example/module1/Module1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ coverege-module/src/test/java/com/coverage/coveregemodule/CoveregeModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coverage.coveregemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoveregeModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
